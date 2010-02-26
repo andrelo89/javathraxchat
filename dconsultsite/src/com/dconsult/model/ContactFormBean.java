478526b99d9db01a5f66e432272bdd516b6f4a23
ivaylo.kovatchev <ivaylo.kovatchev@d3f3cbc2-f916-11de-9656-bb0dda18cfc1>
2010-02-26 15:49:01 +0000

    added persistence

---
+++ dconsultsite/src/com/dconsult/model/ContactFormBean.java
@@ -0,0 +1,85 @@
+package com.dconsult.model;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
+public class ContactFormBean {
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	@Persistent
+	private String companyName;
+	@Persistent
+	private String contactName;
+	@Persistent
+	private String address;
+	@Persistent
+	private String telephone;
+	@Persistent
+	private String mobile;
+	@Persistent
+	private String email;
+	@Persistent
+	private String website;
+	@Persistent
+	private String desiredMoney;
+	public Key getKey() {
+		return key;
+	}
+	public void setKey(Key key) {
+		this.key = key;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getContactName() {
+		return contactName;
+	}
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTelephone() {
+		return telephone;
+	}
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	public String getWebsite() {
+		return website;
+	}
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+	public String getDesiredMoney() {
+		return desiredMoney;
+	}
+	public void setDesiredMoney(String desiredMoney) {
+		this.desiredMoney = desiredMoney;
+	}
+}

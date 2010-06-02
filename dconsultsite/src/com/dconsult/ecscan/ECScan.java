79b316c1af3e8b33b1b3d46a799426be3f42901a
ivaylo.kovatchev <ivaylo.kovatchev@d3f3cbc2-f916-11de-9656-bb0dda18cfc1>
2010-06-02 15:53:24 +0000

    added google search api code

--- dconsultsite/src/com/dconsult/ecscan/ECScan.java
+++ dconsultsite/src/com/dconsult/ecscan/ECScan.java
@@ -5,50 +5,54 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.util.logging.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 @SuppressWarnings("serial")
 public class ECScan extends HttpServlet {
+	private static Logger logger = Logger.getLogger("com.dconsult.ecscan");
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-
-		// This example request includes an optional API key which you will need to
+		// This example request includes an optional API key which you will need
+		// to
 		// remove or replace with your own key.
 		// Read more about why it's useful to have an API key.
 		// The request also includes the userip parameter which provides the end
 		// user's IP address. Doing so will help distinguish this legitimate
 		// server-side traffic from traffic which doesn't come from an end-user.
-		URL url = new URL(
-		    "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&"
-		    + "q=Paris%20Hilton");
+		String query = new String("еврофинансиране".getBytes(Charset.forName("UTF-8")), Charset.forName("UTF-8"));
+		String encoded = URLEncoder.encode(query, "UTF-8");
+		URL url = new URL("http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" + encoded);
 		URLConnection connection = url.openConnection();
 		connection.addRequestProperty("Referer", "http://www.d-consult.net");
-
 		String line;
 		StringBuilder builder = new StringBuilder();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-		while((line = reader.readLine()) != null) {
-		 builder.append(line);
+		while ((line = reader.readLine()) != null) {
+			builder.append(line);
 		}
-
 		try {
 			JSONObject json = new JSONObject(builder.toString());
-			System.out.println(json.toString());
+			JSONArray results = (JSONArray) json.get("results");
+			resp.getOutputStream().println("results:");
+			for (int count = 0; count < results.length(); count++) {
+				resp.getOutputStream().println(results.getJSONObject(count).toString());
+			}
 		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.severe(e.getMessage());
 		}
 		// now have some fun with the results...
-		
-
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
 	}
 }

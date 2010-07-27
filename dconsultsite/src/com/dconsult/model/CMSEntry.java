6a61d0e0fde0ad9a7530e5aef7fdb91b5603bbaf
ivaylo.kovatchev <ivaylo.kovatchev@d3f3cbc2-f916-11de-9656-bb0dda18cfc1>
2010-07-27 20:59:33 +0000

    started CMS servlet

--- dconsultsite/src/com/dconsult/model/CMSEntry.java
+++ dconsultsite/src/com/dconsult/model/CMSEntry.java
@@ -2,10 +2,43 @@ package com.dconsult.model;
 
 import java.util.Date;
 
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+
+@PersistenceCapable
 public class CMSEntry {
+	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Key key;
+	@Persistent
+	private Date creationTime;
+	@Persistent
 	private Date entryCreated;
+	@Persistent
 	private String title;
+	@Persistent
 	private String body;
+	
+	public Date getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(Date creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public Key getKey() {
+		return key;
+	}
+
+	public void setKey(Key key) {
+		this.key = key;
+	}
+	
 	public Date getEntryCreated() {
 		return entryCreated;
 	}
